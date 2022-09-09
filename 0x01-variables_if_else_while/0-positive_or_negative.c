795416f19a5687a75cb30ad6cd33fbe57825d811
Gabriel-J-softmaktech <Gsoftmaktech@gmail.com>
2022-09-09 20:00:44 +0000

    Assign n with random int

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,7 @@
 /* betty style doc for function main goes here */
 int main(void)
 {
-	int n;
-	
+	int n;	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -15,8 +14,5 @@ int main(void)
 		printf("%i is negative\n", n);
 	else
 		printf("%i is zero\n", n);
-	
 	return (0);	
 }
-
-
