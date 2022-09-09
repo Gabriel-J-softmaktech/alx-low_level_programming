047002bc636391c5e6399ff10234fd80a063bbe4
Gabriel-J-softmaktech <Gsoftmaktech@gmail.com>
2022-09-09 23:15:32 +0000

    Print alphabet in reverse mode

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+* main - main block
+* Description: print lowercase alphabet in reverse
+* followed by a new line.
+* Return: 0
+*/
+int main(void)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+		putchar(a);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}
