b00bd9d0fd21e80eaf10c2514a62513d0fb5473e
Gabriel-J-softmaktech <Gsoftmaktech@gmail.com>
2022-09-10 00:12:49 +0000

    single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,14 +3,14 @@
 /**
 * main - main block
 * Descript: points all single numbers
-* all numbers must be separated by , then followed by a new line
+* all numbers must be separated by comma then followed by a new line
 * Return: 0
 */
 int main(void)
 {
 	int c = 0;
 
-	while (c <= 10)
+	while (c < 10)
 	{
 		putchar(48 + c);
 		if (c != 9)
