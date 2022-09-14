01e9f9a066b5179366d12baeb0aeb9e77cac8057
Gabriel-J-softmaktech <Gsoftmaktech@gmail.com>
2022-09-14 00:12:00 +0000

    Task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef ALX_X_INCLUDED
+#define ALX_X_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int print_last_digit(int);
+int _abs(int);
+void jack_bauer(void);
+void times_table(void);
+void print_times_tables(int n);
+int add(int a, int b);
+void num(int r, int n);
+
+#endif
