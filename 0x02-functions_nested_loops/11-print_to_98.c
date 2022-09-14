fb9e3316eb6b3ab9c7dc168333aac0ba32050073
Gabriel-J-softmaktech <Gsoftmaktech@gmail.com>
2022-09-14 02:31:35 +0000

    Print all natural numuber to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_to_98 - prints from any integer to 98
+* @n: integer input
+*
+* Return: void
+*/
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	_putchar("98\n");
+}
