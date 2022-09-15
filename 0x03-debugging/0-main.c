b47d4e56f4ca198e0eb91e85b4816dfddf73f6d2
Gabriel-J-softmaktech <Gsoftmaktech@gmail.com>
2022-09-15 02:34:18 +0000

    Debugging task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+* main - main entry point
+* Description: check if an integer is positive or negative
+* Return: 0
+*/
+
+void positive_or_negative(int i)
+{
+	int i;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (i > 0)
+		printf("%i is positive\n", i);
+	else if (i < 0)
+		printf("%i is negative\n", i);
+	else
+		printf("%i is negative\n", i);
+
+	return (0);
+}
