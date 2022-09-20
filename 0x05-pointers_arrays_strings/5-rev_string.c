9539a4dfeb3cb3705e5c1a1196f304da1920a659
Gabriel-J-softmaktech <Gsoftmaktech@gmail.com>
2022-09-20 01:30:43 +0000

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* rev_string - function reverses a given string
+* @s: string to reverse
+* Return: null
+*/
+
+void rev_string(char *s)
+{
+	int i, p, len = _string(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		p = *(s + i);
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = p;
+	}
+}
+
+/**
+* _strlen - return the length of a given string
+* @s: given string
+* Return: string length
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while(*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
